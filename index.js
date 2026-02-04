5318bae6e1c00bdf9a3e313a4c856dc6621c8735
mzattyz12 <mzattyz12@gmail.com>
2026-02-04 15:10:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+package com.your.package.name // Replace with your package name
 
+import android.app.Application;
+import kotlinx.coroutines.CoroutineScope;
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
+
+class ApplicationClass : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Enable verbose logging for debugging (remove in production)
+        OneSignal.Debug.logLevel = LogLevel.VERBOSE
+        // Initialize with your OneSignal App ID
+        OneSignal.initWithContext(this, "332721bb-21df-4486-8552-3f36a1899772")
+        // Use this method to prompt for push notifications.
+        // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
+        CoroutineScope(Dispatchers.IO).launch {
+            OneSignal.Notifications.requestPermission(true)
+        }
+    }
+}
 admin.initializeApp();
 const db = admin.firestore();
 
