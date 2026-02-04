92a7896663adf347979adc13f90ec49fd9d96cf4
mzattyz12 <mzattyz12@gmail.com>
2026-02-04 12:59:26 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,5 @@
+
+
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
@@ -5,8 +7,15 @@ firebase.initializeApp({
   apiKey: "AIzaSyDdmrhtCWO0Dk_g1ZjVD3W7YvIfbJRi0jI",
   authDomain: "messgsender.firebaseapp.com",
   projectId: "messgsender",
+  storageBucket: "messgsender.firebasestorage.app",
   messagingSenderId: "271051103266",
   appId: "1:271051103266:web:c92c4d33e340bdb3a2fe7b"
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body
+  });
+});
